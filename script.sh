93d43e9927c37ac42b3b952a3ccaaedfd0bf006c
Danilo Moura <danilogmoura@gmail.com>
2022-07-11 02:42:37 -0300

    formatted curl response

--- script.sh
+++ script.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 FILE=$1
 
-echo $(date)
+# No Color
+NC='\033[0m' 
+#Colors
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+
+echo -e "${CYAN}NextDns${NC} ${GREEN}::${NC} Update linked IP..."
 while read line; do
-    echo "${NEXTDNS_HOST}/${line}/${NEXTDNS_USER}"
-    curl --request GET --url "${NEXTDNS_HOST}/${line}/${NEXTDNS_USER}"
-    echo
+    echo -n -e "${NEXTDNS_HOST}/${GREEN}${line}${NC}/${NEXTDNS_USER}\t"
+    curl --silent --request GET --url "${NEXTDNS_HOST}/${line}/${NEXTDNS_USER}" | grep -E "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
 done < $FILE
+echo
